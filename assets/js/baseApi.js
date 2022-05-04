895fec0d45ee9da5bd2450090a30df2cae1695c9
yojaz <mycsus@gmail.com>
2022-05-04 20:50:57 +0800

    completed the development of homepage

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -4,4 +4,16 @@
 $.ajaxPrefilter(function(option) {
     //在发起ajax请求前，拼接字符串
     option.url = 'http://www.liulongbin.top:3007' + option.url;
+    if (option.url.indexOf('/my/') !== -1)
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+        // 全局统一挂载complete
+    option.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            console.log(res);
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+$(function() {
+    getUserinfo();
+    layer = layui.layer;
+    $('#btnLogout').on('click', function() {
+        layer.confirm('Are you sure to log out?', { icon: 3, title: 'Tips' }, function(index) {
+            //do something
+            //清空本地存储的token
+            localStorage.removeItem('token');
+            // 跳转到登录页面
+            location.href = '/login.html';
+            layer.close(index);
+        });
+    })
+})
+
+function getUserinfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layer.msg('获取用户信息失败！');
+            }
+            renderAvatar(res.data);
+        },
+
+
+    })
+}
+
+function renderAvatar(user) {
+    // 1.获取用户名称
+    let name = user.username || user.nickname;
+    // 2.设置欢迎文本
+
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    // 3.按需渲染用户的头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        $('.text-avatar').html(name[0].toUpperCase()).show();
+    }
+}
